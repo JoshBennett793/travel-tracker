00ace43df4e9ba0209f9ee6cba1c554fda91653d
Josh Bennett <joshbennett793@yahoo.com>
2023-08-02 13:15:38 -0500

    Set up store and fetch from api

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,55 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import { getAPIData } from './apiCalls';
 
+function initStore() {
+  const store = {
+    apiKey: {
+      base: 'http://localhost:3001/api/v1/',
+      endpoints: {
+        travelers: 'travelers',
+        trips: 'trips',
+        destinations: 'destinations',
+      },
+    },
+  };
 
-console.log('This is the JavaScript entry file - your code begins here.');
+  return {
+    getAPIKey(endpoint) {
+      const ref = store.apiKey;
+      return `${ref.base}${ref.endpoints[endpoint]}`;
+    },
+
+    getKey(key) {
+      return store[key];
+    },
+
+    setKey(key, value) {
+      store[key] = value;
+    },
+  };
+}
+
+export let store;
+
+window.onload = () => {
+  store = initStore();
+  initApp();
+};
+
+export function initApp() {
+  Promise.all([
+    getAPIData(store.getAPIKey('travelers')),
+    getAPIData(store.getAPIKey('trips')),
+    getAPIData(store.getAPIKey('destinations')),
+  ]).then(values => {
+    console.log(values);
+    const [travelers, trips, destinations] = values;
+    store.setKey('travelers', travelers);
+    store.setKey('trips', trips);
+    store.setKey('destinations', destinations);
+    console.log('travelers', travelers);
+    console.log('trips', trips);
+    console.log('destinations', destinations);
+  });
+}
