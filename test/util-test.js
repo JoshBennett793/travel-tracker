888fd2d4ff1c68ef9106dd8a881ef8828cb744cf
Josh Bennett <joshbennett793@yahoo.com>
2023-08-03 14:19:33 -0500

    Test for calculating total spent

--- test/util-test.js
+++ test/util-test.js
@@ -3,20 +3,62 @@ import { filterTrips } from '../src/model';
 import { sampleTrips } from '../src/data/sampleData';
 
 describe('Should filter the data', () => {
-  let pastTrips, upcomingTrips, pendingTrips;
+  let allTrips, tripsByYear, pastTrips, upcomingTrips, pendingTrips;
 
   beforeEach('init data', () => {
+    allTrips = filterTrips(sampleTrips, 'all', 1);
+    tripsByYear = filterTrips(sampleTrips, 'byYear', 1, '2023');
     pastTrips = filterTrips(sampleTrips, 'past');
     upcomingTrips = filterTrips(sampleTrips, 'upcoming');
     pendingTrips = filterTrips(sampleTrips, 'pending');
+  });
+
+  it('Should return all trips', () => {
+    expect(allTrips).to.deep.equal([
+      {
+        id: 1,
+        userID: 1,
+        destinationID: 3,
+        travelers: 1,
+        date: '2022/09/16',
+        duration: 8,
+        status: 'approved',
+        suggestedActivities: [],
+      },
+      {
+        id: 2,
+        userID: 1,
+        destinationID: 2,
+        travelers: 4,
+        date: '2023/08/30',
+        duration: 3,
+        status: 'approved',
+        suggestedActivities: [],
+      },
+    ]);
+  });
+
+  it('Should return all trips by user id and year', () => {
+    expect(tripsByYear).to.deep.equal([
+      {
+        id: 2,
+        userID: 1,
+        destinationID: 2,
+        travelers: 4,
+        date: '2023/08/30',
+        duration: 3,
+        status: 'approved',
+        suggestedActivities: [],
+      },
+    ]);
   });
 
   it('Should return all past trips', () => {
     expect(pastTrips).to.deep.equal([
       {
         id: 1,
-        userID: 44,
-        destinationID: 49,
+        userID: 1,
+        destinationID: 3,
         travelers: 1,
         date: '2022/09/16',
         duration: 8,
@@ -26,7 +68,7 @@ describe('Should filter the data', () => {
       {
         id: 3,
         userID: 37,
-        destinationID: 15,
+        destinationID: 1,
         travelers: 2,
         date: '2023/03/01',
         duration: 7,
@@ -40,8 +82,8 @@ describe('Should filter the data', () => {
     expect(upcomingTrips).to.deep.equal([
       {
         id: 2,
-        userID: 23,
-        destinationID: 12,
+        userID: 1,
+        destinationID: 2,
         travelers: 4,
         date: '2023/08/30',
         duration: 3,
@@ -55,7 +97,7 @@ describe('Should filter the data', () => {
     expect(upcomingTrips).to.not.deep.include({
       id: 4,
       userID: 50,
-      destinationID: 9,
+      destinationID: 2,
       travelers: 6,
       date: '2023/11/11',
       duration: 10,
@@ -69,7 +111,7 @@ describe('Should filter the data', () => {
       {
         id: 4,
         userID: 50,
-        destinationID: 9,
+        destinationID: 2,
         travelers: 6,
         date: '2023/11/11',
         duration: 10,
